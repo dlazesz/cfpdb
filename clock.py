c927a02a9a7ec70f225a55b5d1bbf90ff86754d7
Balázs Indig <indba@digitus.itk.ppke.hu>
2019-01-05 16:55:05 +0100

    Add Heroku stuff

---
+++ clock.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8, vim: expandtab:ts=4 -*-
+
+from update_cfpdb_on_github import run_update, git_work_dir
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('cron', hour=1)
+def scheduled_job():
+    run_update(git_work_dir)
+
+sched.start()
